388e0eda46f214ba9ba2b2f62feeb85cc57e1986
Md Aminul Islam Shaon <aminul007.ais@gmail.com>
2025-04-27 03:54:44 +0600

    adjust the client.controller

--- controllers/client.controller.ts
+++ controllers/client.controller.ts
@@ -3,6 +3,7 @@ import { clientSchema } from "../schemas/client.schema";
 
 // to get the client all services
 import * as clientService from "../services/client.service";
+import { Client } from "../models/client.model";
 
 export const create = async (req: Request, res: Response) => {
   try {
@@ -16,7 +17,7 @@ export const create = async (req: Request, res: Response) => {
 
 export const getByUser = async (req: Request, res: Response) => {
   try {
-    const creatorId = (req as any).user.id;
+    const creatorId = (req as any).body.creatorId;
     const clients = await clientService.getClientsByCreator(creatorId);
     res.json(clients);
   } catch (error: any) {
@@ -27,9 +28,24 @@ export const getByUser = async (req: Request, res: Response) => {
 export const update = async (req: Request, res: Response) => {
   try {
     const id = parseInt(req.params.id);
-    const creatorId = (req as any).user.id;
+    const { name, email, phone, company, notes, creatorId } = req.body;
+
     const validated = clientSchema.parse({ ...req.body, creatorId });
-    await clientService.updateClient(id, validated, creatorId);
+    
+    const updatedClient = await Client.update(
+      {
+        name,
+        email,
+        phone,
+        company,
+        notes,
+        creatorId, // Take it from body instead of req.user.id
+      },
+      {
+        where: { creatorId: creatorId },
+      }
+    );
+    
     res.status(200).json({ message: "Client updated" });
   } catch (error: any) {
     res.status(400).json({ error: error.message });
@@ -39,7 +55,7 @@ export const update = async (req: Request, res: Response) => {
 export const remove = async (req: Request, res: Response) => {
   try {
     const id = parseInt(req.params.id);
-    const creatorId = (req as any).user.id;
+    const creatorId = (req as any).body.creatorId;
     await clientService.deleteClient(id, creatorId);
     res.status(200).json({ message: "Client deleted" });
   } catch (error: any) {
