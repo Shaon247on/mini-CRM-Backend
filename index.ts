9853e68fd1c033c5a062526df0da6c6a4ff4d7d9
Md Aminul Islam Shaon <aminul007.ais@gmail.com>
2025-04-27 19:00:09 +0600

    deploing a simple index in vercel

--- index.ts
+++ index.ts
@@ -1,44 +1,62 @@
 import express, { Request, Response, NextFunction } from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import { connection } from './postgres/postgres';
-import userRoutes from './routes/user.route';
-import clientRoutes from './routes/client.route';
-
-dotenv.config();
+// import dotenv from 'dotenv';
+// import cors from 'cors';
+// import { connection } from './postgres/postgres';
+// import userRoutes from './routes/user.route';
+// import clientRoutes from './routes/client.route';
+// dotenv.config();
 
 const app = express();
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://mini-crm-frontend-rho.vercel.app'
-];
+// const allowedOrigins = [
+//   'http://localhost:5173',
+//   'https://mini-crm-frontend-rho.vercel.app'
+// ];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true
+// }));
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('🚀 CRM Mini Server is running successfully!');
-});
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('🚀 CRM Mini Server is running successfully!');
+// });
 
-// Routes
-app.use('/api/users', userRoutes);
-app.use('/api', clientRoutes);
+const PORT = 8000
 
-// Server Start
-// const PORT = process.env.PGPORT || 8000;
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get('/about', (req, res) => {
+  res.send('About route 🎉 ')
+})
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+})
+
+// // Routes
+// app.use('/api/users', userRoutes);
+// app.use('/api', clientRoutes);
+
+
+
+// // Server Start
+// const PORT = process.env.PGPORT || 50809;
+// app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
+
 // app.listen(PORT, () => {
 //   console.log(`Server is running on port: ${PORT}`);
 // });
 
-connection();
+// connection();
+
